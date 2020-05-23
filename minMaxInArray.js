22c01e2e6d697b1053c3a173f5738060445c48d6
saweliy <saweliy@users-MacBook-Pro.local>
2020-05-24 01:37:44 +0300

    develop_commit2

--- minMaxInArray.js
+++ minMaxInArray.js
@@ -3,19 +3,19 @@ var arrSize = 30;
 var A = new Array(arrSize);
 //var B = new Array();
 
-for(var i = 0, size = A.length; i < size; i++) {
-  A[i] = Math.floor(Math.random() * (x - y));
+for (var i = 0, size = A.length; i < size; i++) {
+    A[i] = Math.floor(Math.random() * (x - y));
 }
 
-max = A[0]; 
+max = A[0];
 min = A[0];
 
-for(i = 0; i < A.length; i++){
-    if(A[i] > max){
+for (i = 0; i < A.length; i++) {
+    if (A[i] > max) {
         max = A[i];
     }
 
-    if(A[i] < min){
+    if (A[i] < min) {
         min = A[i];
     }
 }
